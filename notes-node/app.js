257e09e3ac3a800373d8c0c63daacc3babce1c3d
aritrapal <aritra.pal@zippr.in>
2017-12-28 19:07:33 +0530

    Creating JSON and accessing them from a file.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,9 +1,11 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+// const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
 const notes = require('./notes.js');
+
 
 // var res = notes.addNotes();
 // console.log(res);
@@ -11,8 +13,8 @@ const notes = require('./notes.js');
 // console.log(_.isString(true));
 // console.log(_.isString('Aritra'));
 
-var filteredarray = _.uniq(['Aritra', 1, 'Aritra', 1, 2, 3, 4]);
-console.log(filteredarray);
+// var filteredarray = _.uniq(['Aritra', 1, 'Aritra', 1, 2, 3, 4]);
+// console.log(filteredarray);
 
 // console.log('Result: ' + notes.add(5, 4));
 
@@ -21,3 +23,31 @@ console.log(filteredarray);
 
 // fs.appendFile('greetings.txt', 'Hello ' + user.username + ' !');
 // fs.appendFile('greetings.txt', `Hello ${user.username}. Your age is ${notes.age}`);
+
+var argv = yargs.argv;
+// var command = process.argv[2];
+var command = argv._[0];
+// console.log('Command', command);
+// console.log('Process', process.argv);
+// console.log('Yargs', argv);
+
+if(command === 'add') {
+    var note = notes.addNote(argv.title, argv.body);
+    if(note)
+        console.log(note);
+    else
+        console.log(`Note is undefined.`);
+
+    // if(typeof note !== 'undefined' && note !== null)
+    //     console.log(note);
+    // else
+    //     console.log(`Note is undefined.`);
+} else if(command == 'list') {
+    notes.getAll();
+} else if(command === 'read') {
+    notes.getNote(argv.title);
+} else if(command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('command not recognised');
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,11 +1,70 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
 module.exports.addNotes = () => {
     console.log('Addnote');
     return 'New note';
 };
 
-module.exports.add = (a, b) => {
-    console.log('Add');
-    return (a + b);
+var fetchNotes = () => {
+    var notes = [];
+    try {
+        var noteString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(noteString);
+    } catch(ex) {   
+        console.log(ex.title);
+    } finally {
+        return notes;
+    }
 };
+
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));    
+};
+
+var addNote = (title, body) => {
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
+
+    // var dupNotes = notes.filter((note) => {
+    //     console.log(`Title: ${title}, Note title: ${note.title}`);
+    //     return note.title === title;
+    // });
+
+    var dupNotes = notes.filter((note) => note.title === title);
+
+    if(dupNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+};
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Getting your note', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing your note', title);
+};
+
+module.exports = {
+    addNote,
+    // getAll: getAll
+    getAll,
+    getNote,
+    removeNote
+};
+// module.exports.add = (a, b) => {
+//     console.log('Add');
+//     return (a + b);
+// };
