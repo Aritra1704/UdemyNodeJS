705452456543ec3b7ac517c84a873c2cb73b0b43
aritrapal <aritra.pal@zippr.in>
2018-01-07 19:25:14 +0530

    Error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -16,15 +16,24 @@ const argv = yargs
 
 // console.log(argv);
 var encodedAddress = encodeURIComponent(argv.address);
+const api_key = 'AIzaSyCjCgTnlvW8SUUC5SGMo0kslwqQmddVg0Q';
 
 request({
     // url: 'http://maps.google.com/maps/api/geocode/json?address=plot%2083%20mega%20hills%20madhapur',
-    url: `http://maps.google.com/maps/api/geocode/json?address=${encodedAddress}`,
+    url: `http://maps.google.com/maps/api/geocode/json?address=${encodedAddress}&api_key=${api_key}`,
     json: true
 }, (error, response, body) => {
-//    console.log(JSON.stringify(response, undefined, 2)); 
-    console.log(`Address: ${body.results[0].formatted_address}`);
+//    console.log(JSON.stringify(body, undefined, 2)); 
+    if(error) {
+        console.log('Unable to connect to Google Servers');
+    } else if(body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address.');
+    } else if(body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
 
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    } else {
+        console.log('Internet issue.');
+    }
 });
