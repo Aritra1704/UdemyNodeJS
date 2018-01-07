30da57530be1422950618e3b7c7c196ab5d6bdc9
aritrapal <aritra.pal@zippr.in>
2018-01-07 15:30:51 +0530

    encoding user input and http basics

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,30 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+// console.log(argv);
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'http://maps.google.com/maps/api/geocode/json?address=plot%2083%20mega%20hills%20madhapur',
+    // url: 'http://maps.google.com/maps/api/geocode/json?address=plot%2083%20mega%20hills%20madhapur',
+    url: `http://maps.google.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-   console.log(body); 
+//    console.log(JSON.stringify(response, undefined, 2)); 
+    console.log(`Address: ${body.results[0].formatted_address}`);
+
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
