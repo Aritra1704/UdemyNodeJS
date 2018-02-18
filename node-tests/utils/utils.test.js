4541b9eb3f23d30aa5dae46dd2d841bb425c9244
aritrapal <aritra.pal@zippr.in>
2018-02-18 12:53:03 +0530

    Basic usage of Expect

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,13 +1,47 @@
 const utils = require('./utils');
 
+const expect = require('expect');
+
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
-    if(res !== 44)
-        throw new Error(`Expected 44, but got ${res}`);
+
+    expect(res).toBe(44);
+    expect(res).toBeA('number');
+    // if(res !== 44)
+    //     throw new Error(`Expected 44, but got ${res}`);
 });
 
 it('should square a number', () => {
     var res = utils.square(15);
-    if(res !== 225)
-        throw new Error(`Expected 225, but got ${res}`);
+    expect(res).toBe(225);
+    expect(res).toBeA('number');
+    // if(res !== 225)
+    //     throw new Error(`Expected 225, but got ${res}`);
+});
+
+it('should expect some values', () => {
+    // expect(12).toNotBe(12);
+    // expect({name: 'Aritra'}).toBe({name: 'Aritra'});
+    // expect({name: 'Aritra'}).toEqual({name: 'Aritra'});
+    // expect({name: 'Aritra'}).toNotEqual({name: 'Aritra'});
+    // expect([2,3,4]).toInclude([2]);
+    // expect([2,3,4]).toExclude([5]);
+    expect({
+        name: 'Aritra',
+        age: '28',
+        locaton: 'Hyderabad'
+    }).toInclude({age: '28'});
+});
+
+it('should verify first and last name', () => {
+    var user = {
+        age: 28,
+        location: 'Hyderabad'
+    }
+
+    var res = utils.setName(user, 'Aritra Pal');
+    expect(res).toInclude({
+        firstName: 'Aritra',
+        lastName: 'Pal'
+    });
 });
