e75d938a32840b3c3a04d6cd478d98c13decd093
aritrapal <aritra.pal@zippr.in>
2018-02-20 18:57:06 +0530

    Providing formatted testing using describe

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -14,29 +14,39 @@ var app = require('./server').app;
 //     .end(done);
 // })
 
+describe('Server', () => {
 
-it('should return hello world response!', (done) => {
-    request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            error: 'Page not found'
-        })
-    })
-    // .expect('Hello world!')
-    .end(done);
-})
-
-it('should return my user object', (done) => {
-    request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            name: 'Aritra',
-            age: 28
+    describe('GET /', () => {
+        it('should return hello world response!', (done) => {
+            request(app)
+            .get('/')
+            .expect(404)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    error: 'Page not found'
+                })
+            })
+            // .expect('Hello world!')
+            .end(done);
         });
-    })
-    .end(done)
+    });
+
+
+    describe('GET /users', () => {
+        it('should return my user object', (done) => {
+            request(app)
+            .get('/users')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    name: 'Aritra',
+                    age: 28
+                });
+            })
+            .end(done)
+        });
+    });
 });
+
+
+
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,41 +1,49 @@
 const utils = require('./utils');
 
 const expect = require('expect');
+describe('Utils', () => {
 
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
+        
+            expect(res).toBe(44);
+            expect(res).toBeA('number');
+            // if(res !== 44)
+            //     throw new Error(`Expected 44, but got ${res}`);
+        });
+    });
 
-    expect(res).toBe(44);
-    expect(res).toBeA('number');
-    // if(res !== 44)
-    //     throw new Error(`Expected 44, but got ${res}`);
-});
-
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+    
+    
+    it('should async add two numbers', (done) => {
+        utils.asyncAdd(4, 3, (sum) => {
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        });
+    });
+    
+    it('should square a number', () => {
+        var res = utils.square(15);
+        expect(res).toBe(225);
+        expect(res).toBeA('number');
+        // if(res !== 225)
+        //     throw new Error(`Expected 225, but got ${res}`);
+    });
+    
+    it('should async square a number', (done) => {
+        utils.asyncSquare(3, (res) => {
+            expect(res).toBe(9).toBeA('number');
+            done();
+        })
+    
+        
+        // if(res !== 225)
+        //     throw new Error(`Expected 225, but got ${res}`);
     });
 });
 
-it('should square a number', () => {
-    var res = utils.square(15);
-    expect(res).toBe(225);
-    expect(res).toBeA('number');
-    // if(res !== 225)
-    //     throw new Error(`Expected 225, but got ${res}`);
-});
 
-it('should async square a number', (done) => {
-    utils.asyncSquare(3, (res) => {
-        expect(res).toBe(9).toBeA('number');
-        done();
-    })
-
-    
-    // if(res !== 225)
-    //     throw new Error(`Expected 225, but got ${res}`);
-});
 
 it('should expect some values', () => {
     // expect(12).toNotBe(12);
