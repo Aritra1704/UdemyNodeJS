4b8923d9fb1b0f9575f2002b5222de10b45145e1
aritrapal <aritra.pal@zippr.in>
2018-02-27 18:26:29 +0530

    Mongoose schema basics

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -6,13 +6,18 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 //save new something
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true
     }, 
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
@@ -26,14 +31,37 @@ var Todo = mongoose.model('Todo', {
 //     console.log('Unable to save Todo');
 // });
 
-var challTodo = new Todo({
-    text: 'Challenge2 Todo',
-    completed: true,
-    completedAt: 123
+// var challTodo = new Todo({
+//     text: 'Challenge2 Todo',
+//     completed: true,
+//     completedAt: 123
+// });
+
+// var challTodo = new Todo({
+//     text: '  Edit this video   '
+// });
+
+// challTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('Unable to save challenge todo');
+// })
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 1
+    }
 });
 
-challTodo.save().then((doc) => {
+var testUser = new User({
+    // email: 'aritra@test.com'
+});
+
+testUser.save().then((doc) => {
     console.log(JSON.stringify(doc, undefined, 2));
 }, (e) => {
-    console.log('Unable to save challenge todo');
-})
+    console.log('Unable to save user', e);
+});
